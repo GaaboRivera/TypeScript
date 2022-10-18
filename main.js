0171ced695540b53865bb9e17b4f7dbbc6976c00
GaaboRivera <gabo.grm27@gmail.com>
2022-10-18 11:26:06 -0500

    ES6

--- main.js
+++ main.js
@@ -1,266 +1,50 @@
 "use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 (function () {
-    var a = 10;
-    var b = 10;
-    function sayHello(msg) {
-        console.log(msg);
+    var avengers = {
+        nick: 'Samuel L. Jackson',
+        ironman: 'Robert Downey Jr.',
+        vision: 'Paul Bettany',
+        activo: true,
+        poder: 1500.123123
+    };
+    var printAvenger = function (_a) {
+        var ironman = _a.ironman, resto = __rest(_a, ["ironman"]);
+        console.log(ironman, resto);
+    };
+    var avengersArr = ['Cap. América', true, 150.15];
+    var capitan = avengersArr[0], ironman = avengersArr[1], seriaUnNumero = avengersArr[2];
+})();
+(function () {
+    var ironman = {
+        name: 'Ironman',
+        weapon: 'Armorsuit'
+    };
+    var captainAmerica = {
+        name: 'Capitán América',
+        weapon: 'Escudo'
+    };
+    var thor = {
+        name: 'Thor',
+        weapon: 'Mjolnir'
+    };
+    var avengers = [ironman, thor, captainAmerica];
+    for (var _i = 0, avengers_1 = avengers; _i < avengers_1.length; _i++) {
+        var avenger = avengers_1[_i];
+        console.log(avenger.name, avenger.weapon);
     }
-    sayHello('Hola Mundo');
 })();
 (function () {
-    var sumar = function (a, b) { return a + b; };
-    var contar = function (heroes) {
-        return heroes.length;
-    };
-    var superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-    contar(superHeroes);
-    var llamarBatman = function (llamar) {
-        if (llamar === void 0) { llamar = true; }
-        if (llamar) {
-            console.log("Batiseñal activada");
-        }
-    };
-    llamarBatman();
-    var unirheroes = function () {
-        var personas = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            personas[_i] = arguments[_i];
-        }
-        return personas.join(", ");
-    };
-    var noHaceNada = function (numero, texto, booleano, arreglo) { };
-    var noHaceNadaTampoco;
-    noHaceNadaTampoco = noHaceNada;
-})();
-(function () {
-    var addNumbers = function (a, b) { return a + b; };
-    var greet = function (name) { return "Hola ".concat(name); };
-    var saveTheWorld = function () { return "El mundo esta salvado"; };
-    var myFuncNumber;
-    myFuncNumber = addNumbers;
-    console.log(myFuncNumber(1, 2));
-    var myFuncString;
-    myFuncString = greet;
-    console.log(myFuncString('Gabo'));
-    var myFuncString2;
-    myFuncString2 = saveTheWorld;
-    console.log(myFuncString2());
-})();
-(function () {
-    var hero = 'Flash';
-    function returnName() {
-        return hero;
-    }
-    var activeBatisignal = function () {
-        return 'Batiseñal activada';
-    };
-    console.log(typeof activeBatisignal);
-})();
-(function () {
-    var fullName = function (firtName, pathName) {
-        return "".concat(firtName, " ").concat(pathName || 'No lastName');
-    };
-    var name = fullName('Gabo');
-    console.log(name);
-})();
-(function () {
-    var fullName = function (firtName, pathName, upper) {
-        if (upper === void 0) { upper = false; }
-        return upper ? "".concat(firtName, " ").concat(pathName).toUpperCase() : "".concat(firtName, " ").concat(pathName);
-    };
-    var name = fullName('Gabo', 'Rivera', true);
-    console.log(name);
-})();
-(function () {
-    var fullname = function (firstName) {
-        var restArgs = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            restArgs[_i - 1] = arguments[_i];
-        }
-        return "".concat(firstName, " ").concat(restArgs.join(' '));
-    };
-    var superman = fullname('Clark', 'Joseph', 'Kent');
-    console.log(superman);
-})();
-(function () {
-    var fullName = function (firtName, pathName) {
-        return "".concat(firtName, " ").concat(pathName);
-    };
-    var name = fullName('Gabo', 'Rivera');
-    console.log(name);
-})();
-(function () {
-    var batimovil = {
-        carroceria: "Negra",
-        modelo: "6x6",
-        antibalas: true,
-        pasajeros: 4
-    };
-    var bumblebee = {
-        carroceria: "Amarillo con negro",
-        modelo: "4x2",
-        antibalas: true,
-        pasajeros: 4,
-        disparar: function () {
-            console.log("Disparando");
-        }
-    };
-    var villanos = [
-        {
-            nombre: "Lex Luthor",
-            edad: 54,
-            mutante: false
-        },
-        {
-            nombre: "Erik Magnus Lehnsherr",
-            edad: 49,
-            mutante: true
-        },
-        {
-            nombre: "James Logan",
-            edad: undefined,
-            mutante: true
-        }
-    ];
-    var charles = {
-        poder: "psiquico",
-        estatura: 1.78
-    };
-    var apocalipsis = {
-        lider: true,
-        miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"]
-    };
-    var mystique;
-    mystique = charles;
-    mystique = apocalipsis;
-})();
-(function () {
-    var flash = {
-        name: 'Barry Alen',
-        age: 24,
-        powers: ['supervelocidad', 'viajar en el tiempo']
-    };
-    flash = {
-        name: 'Barry Alen',
-        age: 24,
-        powers: ['supervelocidad', 'viajar en el tiempo'],
-        getName: function () {
-            return this.name;
-        }
-    };
-    console.log(flash);
-})();
-(function () {
-    var aquaman = {
-        name: 'Alen Tuds',
-        age: 45,
-        powers: ['supervelocidad']
-    };
-    var superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['supervelocidad']
-    };
-})();
-(function () {
-    var myCustomVariable = 'Gabo';
-    console.log(typeof myCustomVariable);
-    myCustomVariable = 10;
-    console.log(typeof myCustomVariable);
-    myCustomVariable = {
-        name: 'Gabo',
-        age: 33,
-        powers: ['code']
-    };
-    console.log(typeof myCustomVariable);
-})();
-(function () {
-    var avenger = 132;
-    var exists;
-    var power;
-    avenger = 'Dr strange';
-    console.log(avenger.charAt(0));
-    console.log(avenger.charAt(0));
-    avenger = 150.485168854;
-    console.log(avenger.toFixed(3));
-    console.log(avenger.toFixed(2));
-    console.log(exists);
-    console.log(power);
-})();
-(function () {
-    var numStrBool = [1, 2, 3, '4', 5, 6, 7, 8, 9, 10, false];
-    numStrBool.push(true);
-    console.log(numStrBool);
-    var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    console.log(numbers);
-    var strings = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    console.log(strings);
-})();
-(function () {
-    var isSuperman = true;
-    var isBatman = false;
-    isSuperman = (isBatman) ? true : false;
-    console.log({ isSuperman: isSuperman });
-})();
-(function () {
-    var AudioLevel;
-    (function (AudioLevel) {
-        AudioLevel[AudioLevel["min"] = 1] = "min";
-        AudioLevel[AudioLevel["medium"] = 2] = "medium";
-        AudioLevel[AudioLevel["max"] = 10] = "max";
-    })(AudioLevel || (AudioLevel = {}));
-    var currentAudio = AudioLevel.medium;
-    console.log(currentAudio);
-    console.log(AudioLevel);
-})();
-(function () {
-    var error = function (msg) {
-        throw new Error(msg);
-    };
-    error('auxilio');
-    console.log('ya no llega aqui');
-})();
-(function () {
-    var nada = undefined;
-    console.log(nada);
-})();
-(function () {
-    var avengers = 10;
-    console.log(avengers);
-    var villians = 20;
-    if (avengers < villians) {
-        console.log('estamos en problemas');
-    }
-    else {
-        console.log('nos salvamos');
-    }
-    avengers = Number('55A');
-    console.log({ avengers: avengers });
-})();
-(function () {
-    var _a;
-    var batman = 'batman';
-    var linternaVerde = "Linterna Verde";
-    var superman = "Superman";
-    console.log(batman.toLocaleUpperCase());
-    console.log("I'm ".concat(batman));
-    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toLocaleUpperCase()) || 'no da el ancho');
-})();
-(function () {
-    var hero = ['dr strange', 100, true];
-    hero[0] = 'Ironman';
-    hero[1] = 50;
-    hero[2] = false;
-    console.log(hero);
-})();
-(function () {
-    function callBatman() {
-        return;
-    }
-    var callSuperman = function () {
-        return;
-    };
-    var a = callBatman();
-    console.log(a);
+    console.log('let');
 })();
 //# sourceMappingURL=main.js.map
